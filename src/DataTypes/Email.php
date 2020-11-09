bbae5ac464e1d7c31a6e09df19d9a28052504436
xbojer <fbojer@gmail.com>
2020-11-09 20:01:07 +0100

    Update Email.php
    Uzupelnienie email

--- src/DataTypes/Email.php
+++ src/DataTypes/Email.php
@@ -6,5 +6,18 @@ namespace michalrokita\BlueMediaSDK\DataTypes;
 
 class Email
 {
+    protected $_email;
 
+    public function __construct(string $email)
+    {
+        $this->_email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->_email;
+    }
 }
