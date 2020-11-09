ed714ed6e98162e0b32b2f2cab906b417f38d4d1
xbojer <fbojer@gmail.com>
2020-11-09 20:00:07 +0100

    Update Description.php
    Przesłanie wartości do Description

--- src/DataTypes/Transactions/Description.php
+++ src/DataTypes/Transactions/Description.php
@@ -6,5 +6,18 @@ namespace michalrokita\BlueMediaSDK\DataTypes\Transactions;
 
 class Description
 {
+    protected $_description;
 
+    public function __construct(string $description)
+    {
+        $this->_description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->_description;
+    }
 }
